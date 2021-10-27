d31010f8bda7b2a5ca0bf7095fd8937789522fd1
Badri Vishal <bv7887393@gmail.com>
2021-10-27 08:58:53 +0530

    Create main.cpp

---
+++ input-output/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Integer
+  int n;
+  cin >> n;
+  // String
+  string s;
+  cin >> s
+  // Output the integer
+  cout << n << endl;
+  // Output the String
+  cout << s << endl;
+}
